940366f3cf7ef516335a973f3f47a386290fd6d3
yumisaitos <giovanna.yumi@ifsp.edu.br>
2024-08-07 10:37:15 -0300

    modificado2

--- 2024-08-07/insert-music.js
+++ 2024-08-07/insert-music.js
@@ -3,7 +3,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-    const data = {
+    const data = [{
         name: 'RED' ,
         singer: 'Taylor Swift' ,
         name: 'Ciclano' ,
@@ -12,7 +12,18 @@ async function main() {
         single: 'Yes' ,
         lancamento: new Date("2012-10-22"),
         duracao: new Date('3:41')
-    };
+    },
+
+    {
+        name: 'RED' ,
+        singer: 'Taylor Swift' ,
+        name: 'Ciclano' ,
+        album: 'Red' ,
+        gender: 'pop','rock pop','country' ,
+        single: 'Yes' ,
+        lancamento: new Date("2012-10-22"),
+        duracao: new Date('3:41')
+    }];
     await prisma.music.create({ data });
     console.log("Cadastro realizado com sucesso!");
 }
